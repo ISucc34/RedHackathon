387c540cb1430b6a46ee6c77bb8f1c0099abe378
nkashif1 <nawalkashif11@gmail.com>
2025-10-18 18:33:53 -0500

    hi

--- script.js
+++ script.js
@@ -5,7 +5,48 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //url is where leaflet is getting the images from
 //attribution is giving credit to the source of the images; and then we add it to the map
 
-L.marker([29.7604, -95.3698]).addTo(map)
+/*L.marker([29.7604, -95.3698]).addTo(map)
     .bindPopup('Test Marker - Houston')
-    .openPopup();
+    .openPopup();*/
+
+
 let earthquakeData; //hold earthquake info
+function getEarthquakes() {
+    fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson')//all earthquakes in the past 24 hours
+        .then(response => response.json()) //convert response from text format to useable javascript object
+        .then(data => {
+            console.log('Earthquake data:', data);  // See data in console
+            displayEarthquakes(data);
+        })
+        .catch(error => { //runs if something goes wrong
+            console.error('Error fetching earthquakes:', error);
+        });
+}
+
+// Function to display earthquakes as markers
+function displayEarthquakes(data) {
+    let earthquakes = data.features;  // Get array of earthquakes
+    
+    console.log(`Found ${earthquakes.length} earthquakes`);
+    
+    earthquakes.forEach(function(quake) {
+        // Get earthquake details
+        let coords = quake.geometry.coordinates;
+        let lat = coords[1];  // Latitude
+        let lon = coords[0];  // Longitude
+        let mag = quake.properties.mag;  // Magnitude
+        let place = quake.properties.place;  // Location name
+        let time = new Date(quake.properties.time);  // Time of earthquake
+        
+        // Create marker for each earthquake
+        L.marker([lat, lon]).addTo(map)
+            .bindPopup(`
+                <b>Magnitude:</b> ${mag}<br>
+                <b>Location:</b> ${place}<br>
+                <b>Time:</b> ${time.toLocaleString()}
+            `);
+    });
+}
+
+// Load earthquakes when page loads
+getEarthquakes();
