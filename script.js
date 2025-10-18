39e5afca1967199bec0f6506088b127fa2a02fcd
nkashif1 <nawalkashif11@gmail.com>
2025-10-18 18:17:12 -0500

    hi

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //url is where leaflet is getting the images from
 //attribution is giving credit to the source of the images; and then we add it to the map
 
-
+L.marker([29.7604, -95.3698]).addTo(map)
+    .bindPopup('Test Marker - Houston')
+    .openPopup();
 let earthquakeData; //hold earthquake info
